021a20a78fb27b8bb2083b5645e9446e468017cd
Cristian Molina <megatux@gmail.com>
2014-08-10 22:30:56 -0300

    Add API controller with authentication

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,6 +1,5 @@
-class Api::V1::PostsController < ApplicationController
-	respond_to :json
-	skip_before_filter :authenticate_user
+class Api::V1::PostsController < Api::V1::ApiController
+  skip_before_filter :authenticate, only: [:show, :index]
 
 	def index
 		render json: { posts: Post.all }
