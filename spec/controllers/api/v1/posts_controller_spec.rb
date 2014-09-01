792b3009d1d404d36d7d6a7cba0f894ac61769ea
alemata <amataloni@gmail.com>
2014-09-01 14:51:58 -0300

    Remove spaces.

--- spec/controllers/api/v1/posts_controller_spec.rb
+++ spec/controllers/api/v1/posts_controller_spec.rb
@@ -2,159 +2,159 @@ require 'rails_helper'
 
 describe Api::V1::PostsController do
 
-	let!(:user) { create(:user) }
-	let!(:a_post) { create(:post) }
-	let!(:non_existing_post_id) { Post.order(:id).last.id + 1 }
+  let!(:user) { create(:user) }
+  let!(:a_post) { create(:post) }
+  let!(:non_existing_post_id) { Post.order(:id).last.id + 1 }
 
-	describe "GET #index" do
-		let!(:other_post) { create(:post) }
+  describe "GET #index" do
+    let!(:other_post) { create(:post) }
 
-		it "renders the json index" do
-			get :index, format: :json
+    it "renders the json index" do
+      get :index, format: :json
       check_success_json_response
-			expect(json).to have_key('posts')
-			expect(json['posts'].size).to eq(2)
-		end
+      expect(json).to have_key('posts')
+      expect(json['posts'].size).to eq(2)
+    end
 
-		it "paginate items" do
-			get :index, page: 1, per_page: 1, format: :json
+    it "paginate items" do
+      get :index, page: 1, per_page: 1, format: :json
       check_success_json_response
-			expect(json).to have_key('posts')
-			expect(json['posts'].size).to eq(1)
-		end
+      expect(json).to have_key('posts')
+      expect(json['posts'].size).to eq(1)
+    end
 
-		it "filter items with simple query" do
+    it "filter items with simple query" do
       post_to_search = create(:post, title: "To Search")
-			get :index, page: 1, query: post_to_search.title, format: :json
+      get :index, page: 1, query: post_to_search.title, format: :json
       check_success_json_response
-			expect(json).to have_key('posts')
-			expect(json['posts'].size).to eq(1)
+      expect(json).to have_key('posts')
+      expect(json['posts'].size).to eq(1)
       expect(json['posts'][0]).to eq(active_record_to_json post_to_search)
       expect(json['posts']).to include(active_record_to_json post_to_search)
-		end
+    end
 
-		it "filter items with simple query_2" do
+    it "filter items with simple query_2" do
       post_to_search = create(:post, title: "To Search")
       post_to_search_2 = create(:post, body: "To Search 2")
-			get :index, page: 1, query: "To Sea", format: :json
+      get :index, page: 1, query: "To Sea", format: :json
       check_success_json_response
-			expect(json).to have_key('posts')
-			expect(json['posts'].size).to eq(2)
+      expect(json).to have_key('posts')
+      expect(json['posts'].size).to eq(2)
       expect(json['posts']).to include(active_record_to_json post_to_search)
       expect(json['posts']).to include(active_record_to_json post_to_search_2)
-		end
+    end
 
-		it "filter items with simple query and order by title" do
+    it "filter items with simple query and order by title" do
       post_to_search = create(:post, title: "To Search")
       post_to_search_2 = create(:post, title: "AAA", body: "To Search 2")
-			get :index, page: 1, query: "To Sea", order_by: "title", order_type: "asc", format: :json
+      get :index, page: 1, query: "To Sea", order_by: "title", order_type: "asc", format: :json
       check_success_json_response
-			expect(json).to have_key('posts')
-			expect(json['posts'].size).to eq(2)
+      expect(json).to have_key('posts')
+      expect(json['posts'].size).to eq(2)
       expect(json['posts'][1]).to eq(active_record_to_json post_to_search)
       expect(json['posts'][0]).to eq(active_record_to_json post_to_search_2)
-		end
-    
-	end
+    end
 
-	describe "GET #show" do
+  end
 
-		it "renders the json post" do
-			get :show, id: a_post, format: :json
+  describe "GET #show" do
+
+    it "renders the json post" do
+      get :show, id: a_post, format: :json
       check_success_json_response
-			expect(json).to eq(active_record_to_json a_post)
-		end
+      expect(json).to eq(active_record_to_json a_post)
+    end
 
-		context "post does not exists" do
-			it "fails with Not-Found" do
-				get :show, id: non_existing_post_id, format: :json
-				expect(response).to have_http_status(404)
-			end
-		end
-	end
+    context "post does not exists" do
+      it "fails with Not-Found" do
+        get :show, id: non_existing_post_id, format: :json
+        expect(response).to have_http_status(404)
+      end
+    end
+  end
 
-	describe "POST to create a Post" do
-		let!(:unsaved_post) { build(:post) }
+  describe "POST to create a Post" do
+    let!(:unsaved_post) { build(:post) }
 
-		context "without a valid authentication token" do
-			it "fails" do
-				post :create, post: unsaved_post.attributes, format: :json
-				expect(response).to have_http_status(401)
-			end
-		end
+    context "without a valid authentication token" do
+      it "fails" do
+        post :create, post: unsaved_post.attributes, format: :json
+        expect(response).to have_http_status(401)
+      end
+    end
 
-		context "with a valid authentication token" do
-		  before { set_http_auth user.api_auth_token }
+    context "with a valid authentication token" do
+      before { set_http_auth user.api_auth_token }
 
-			it "succeed" do
-				post :create, post: unsaved_post.attributes, format: :json
-				expect(response).to have_http_status(:success)
-			end
-		end
-	end
+      it "succeed" do
+        post :create, post: unsaved_post.attributes, format: :json
+        expect(response).to have_http_status(:success)
+      end
+    end
+  end
 
-	describe "DELETE to remove a Post" do
+  describe "DELETE to remove a Post" do
 
-		it "fails when not authenticated" do
-			delete :destroy, id: a_post.id, format: :json
-			expect(response).to have_http_status(401)
-		end
+    it "fails when not authenticated" do
+      delete :destroy, id: a_post.id, format: :json
+      expect(response).to have_http_status(401)
+    end
 
-		context "user is not the creator" do
-			before { set_http_auth user.api_auth_token }
+    context "user is not the creator" do
+      before { set_http_auth user.api_auth_token }
 
-			it "fails with Forbidden status" do
-				delete :destroy, id: a_post.id, format: :json
-				expect(response).to have_http_status(403)
-			end
-		end
+      it "fails with Forbidden status" do
+        delete :destroy, id: a_post.id, format: :json
+        expect(response).to have_http_status(403)
+      end
+    end
 
-		context "user is the post's creator" do
-			before { set_http_auth a_post.user.api_auth_token }
+    context "user is the post's creator" do
+      before { set_http_auth a_post.user.api_auth_token }
 
-			it "succeed" do
-				expect {
-					delete :destroy, id: a_post.id
-					expect(response).to have_http_status(204)
-				}.to change(Post, :count).by(-1)
-			end
-		end
+      it "succeed" do
+        expect {
+          delete :destroy, id: a_post.id
+          expect(response).to have_http_status(204)
+        }.to change(Post, :count).by(-1)
+      end
+    end
 
-		context "post does not exists" do
-			before { set_http_auth a_post.user.api_auth_token }
+    context "post does not exists" do
+      before { set_http_auth a_post.user.api_auth_token }
 
-			it "fails with Not-Found" do
-				delete :destroy, id: non_existing_post_id
-				expect(response).to have_http_status(404)
-			end
-		end
+      it "fails with Not-Found" do
+        delete :destroy, id: non_existing_post_id
+        expect(response).to have_http_status(404)
+      end
+    end
 
-		describe "PUT #update" do
+    describe "PUT #update" do
 
-			context "not authenticated" do
-				it "fails with Forbidden status" do
-					put :update, id: a_post.id, format: :json
-					expect(response).to have_http_status(401)
-				end
-			end
+      context "not authenticated" do
+        it "fails with Forbidden status" do
+          put :update, id: a_post.id, format: :json
+          expect(response).to have_http_status(401)
+        end
+      end
 
-			context "non-existing Post" do
-				before { set_http_auth a_post.user.api_auth_token }
+      context "non-existing Post" do
+        before { set_http_auth a_post.user.api_auth_token }
 
-				it "fails with Not-Found" do
-					put :update, id: non_existing_post_id, post: a_post.attributes, format: :json
-					expect(response).to have_http_status(404)
-				end
-			end
+        it "fails with Not-Found" do
+          put :update, id: non_existing_post_id, post: a_post.attributes, format: :json
+          expect(response).to have_http_status(404)
+        end
+      end
 
-			context "valid request" do
-				before { set_http_auth a_post.user.api_auth_token }
+      context "valid request" do
+        before { set_http_auth a_post.user.api_auth_token }
 
-				it "updates the post" do
-					put :update, id: a_post.id, post: a_post.attributes, format: :json
-					expect(response).to have_http_status(204)
-				end
-			end
-		end
-	end
+        it "updates the post" do
+          put :update, id: a_post.id, post: a_post.attributes, format: :json
+          expect(response).to have_http_status(204)
+        end
+      end
+    end
+  end
 end
